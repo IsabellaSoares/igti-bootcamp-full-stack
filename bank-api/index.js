b0586f0ef27a810b3f4be10b6e62d417b560d561
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-13 19:06:36 -0300

    feat: add post request

--- bank-api/index.js
+++ bank-api/index.js
@@ -1,5 +1,25 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => response.status(200).send('Hello world!'));
+
+app.post('/account', (request, response) => {
+  let body = request.body;
+  let data = JSON.parse(fs.readFileSync('./json/accounts.json', 'utf8'));
+
+  data.accounts.push(body);
+
+  fs.writeFileSync(
+    './json/accounts.json',
+    JSON.stringify(data, null, 2),
+    (err) => response.status(500).send('Internal server error.')
+  );
+
+  response.status(200).send('Success!');
+});
 
 app.listen(3000);
