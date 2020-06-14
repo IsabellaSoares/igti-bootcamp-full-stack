423995d2569b5c040da67065febd63c85e2eeffe
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-13 22:32:39 -0300

    feat: add get method

--- bank-api/index.js
+++ bank-api/index.js
@@ -1,34 +1,44 @@
 const fs = require('fs');
 const express = require('express');
 
+const FILENAME = './json/accounts.json';
+
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (request, response) => response.status(200).send('Hello world!'));
 
+app.get('/accounts', (request, response) => {
+  try {
+    let data = JSON.parse(fs.readFileSync(FILENAME, 'utf8'));
+    response.status(200).send({ accounts: data.accounts });
+  } catch (err) {
+    response.status(400).send({ error: err.message });
+  }
+});
+
 app.post('/account', (request, response) => {
   let body = request.body;
-  let data = JSON.parse(fs.readFileSync('./json/accounts.json', 'utf8'));
-
-  data.accounts.push({ id: data.nextId, ...body });
-  data.nextId++;
+  let data = {};
 
   try {
-    fs.writeFileSync(
-      './json/accounts.json',
-      JSON.stringify(data, null, 2),
-      (err) => response.status(500).send('Internal server error.')
+    data = JSON.parse(fs.readFileSync(FILENAME, 'utf8'));
+    data.accounts.push({ id: data.nextId, ...body });
+    data.nextId++;
+
+    fs.writeFileSync(FILENAME, JSON.stringify(data, null, 2), (err) =>
+      response.status(400).send({ error: err.message })
     );
 
     response.status(200).send('Success!');
   } catch (err) {
-    response.status(500).send('Internal server error.');
+    response.status(400).send({ error: err.message });
   }
 });
 
 app.listen(3000, () => {
-  let exists = fs.existsSync('./json/accounts.json');
+  let exists = fs.existsSync(FILENAME);
 
   if (!exists) {
     let initialJson = {
@@ -36,9 +46,6 @@ app.listen(3000, () => {
       accounts: [],
     };
 
-    fs.writeFileSync(
-      './json/accounts.json',
-      JSON.stringify(initialJson, null, 2)
-    );
+    fs.writeFileSync(FILENAME, JSON.stringify(initialJson, null, 2));
   }
 });
