9f59494b04eed98d974b44e6e2e92d2a7f4e5a49
IsabellaSoares <isabella.soares.28@gmail.com>
2020-06-17 10:07:00 -0300

    refactor: change to Materialize

--- trabalho-prático-03/react-salario/src/App.js
+++ trabalho-prático-03/react-salario/src/App.js
@@ -1,27 +1,9 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { TextField, Typography } from '@material-ui/core';
+import 'materialize-css';
 
 import { calculateSalaryFrom } from './salary.js';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: '22%',
-  },
-  formGroup: {
-    display: 'flex',
-    justifyContent: 'space-between',
-  },
-}));
-
 const App = () => {
-  const classes = useStyles();
   const [values, setValues] = useState({
     salary: '',
     baseINSS: '',
@@ -37,71 +19,45 @@ const App = () => {
   };
 
   return (
-    <>
-      <Typography variant="h3">React Salário</Typography>
-      <div className={classes.root}>
-        <div>
-          <TextField
-            id="standard-full-width"
-            label="Salário Bruto"
-            placeholder="Salário Bruto"
-            style={{ margin: 8 }}
-            fullWidth
-            margin="normal"
-            type="number"
-            value={values.salary}
-            onChange={handleSalaryChange}
-          />
-          <div className={classes.formGroup}>
-            <TextField
-              id="margin-normal"
-              label="Base INSS"
-              placeholder="Base INSS"
-              className={classes.textField}
-              margin="normal"
-              disabled
-              value={values.baseINSS}
+    <div className="row">
+      <form className="col s8 offset-s2">
+        <div className="row">
+          <div className="input-field col s12">
+            <input
+              id="standard-full-width"
+              type="number"
+              value={values.salary}
+              onChange={handleSalaryChange}
             />
-            <TextField
-              id="margin-normal"
-              label="Desconto INSS"
-              placeholder="Desconto INSS"
-              className={classes.textField}
-              margin="normal"
-              disabled
-              value={values.discountINSS}
-            />
-            <TextField
-              id="margin-normal"
-              label="Base IRPF"
-              placeholder="Base IRPF"
-              className={classes.textField}
-              margin="normal"
-              disabled
-              value={values.baseIRPF}
-            />
-            <TextField
-              id="margin-normal"
-              label="Desconto IRPF"
-              placeholder="Desconto IRPF"
-              className={classes.textField}
-              margin="normal"
-              disabled
-              value={values.discountIRPF}
-            />
+            <label for="disabled">Salário Bruto</label>
           </div>
-          <TextField
-            id="margin-normal"
-            label="Salário Líquido"
-            placeholder="Salário Líquido"
-            className={classes.textField}
-            margin="normal"
-            disabled
-            value={values.netSalary}
-          />
         </div>
-      </div>
-    </>
+        <div className="row">
+          <div className="input-field col s3">
+            <input id="baseINSS" disabled value={values.baseINSS} />
+            <label for="baseINSS">Base INSS</label>
+          </div>
+          <div className="input-field col s3">
+            <input id="discountINSS" disabled value={values.discountINSS} />
+            <label for="discountINSS">Desconto INSS</label>
+          </div>
+          <div className="input-field col s3">
+            <input id="baseIRPF" disabled value={values.baseIRPF} />
+            <label for="baseIRPF">Base IRPF</label>
+          </div>
+          <div className="input-field col s3">
+            <input id="discountIRPF" disabled value={values.discountIRPF} />
+            <label for="discountIRPF">Desconto IRPF</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s3">
+            <input id="netSalary" disabled value={values.netSalary} />
+            <label for="netSalary">Salário Líquido</label>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 };
 
